dbbd5de223e47ddd9b4864d12c6c4b40f31ec1b9
Joshua Sullivan <joshuasullivan@fb.com>
2022-08-02 23:45:20 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+GITHUB_USERNAME="dantechristmasfb"
+
+echo "[INSTALL] Ohmyzsh"
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+echo "[INSTALL] Chezmoi"
+export BINDIR=$HOME/.local/bin
+sh -c "$(curl -fsLS chezmoi.io/get)"
+
+echo "[SETUP] Chezmoi"
+$BINDIR/chezmoi init $GITHUB_USERNAME
+
+echo "[APPLY] apply dotfiles"
+$BINDIR/chezmoi apply
