50e7c57a3d55c70724aabb0f4c491b6d21f587f0
dantechristmasfb <89796535+dantechristmasfb@users.noreply.github.com>
2022-10-25 11:56:51 -0600

    Update install.sh
    fix github config

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ set -e
 
 GITHUB_USERNAME="dantechristmasfb"
 
+echo "[CONFIG] GitHub Email and Username"
+git config --global user.email "joshuasullivan@meta.com"
+git config --global user.name "Joshua Sullivan"
+
 echo "[INSTALL] Ohmyzsh"
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
@@ -16,6 +20,3 @@ $BINDIR/chezmoi init $GITHUB_USERNAME
 echo "[APPLY] apply dotfiles"
 $BINDIR/chezmoi apply
 
-source $HOME/.zshrc
-git config --global user.name "Josh Sullivan"
-git config --global user.email "joshuasullivan@fb.com"
